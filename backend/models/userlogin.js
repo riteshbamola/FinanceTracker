1db36856780cefec3affbc557db5149cf442089d
Ritesh Bamola <rishubamola01@gmail.com>
2024-08-08 17:18:52 +0530

    First-commit

---
+++ backend/models/userlogin.js
@@ -0,0 +1,23 @@
+const { createHmac, randomBytes } = require('crypto');
+const mongoose = require('mongoose');
+const { createToken } = require('../services/auth'); // Ensure this path is correct
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('User', userSchema);
